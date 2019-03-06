37b046d22f0cf52c91cf2857cf7eb29440e264f8
gromax <gomaxx@gmail.com>
2019-03-06 16:16:37 +0100

    fichiers include php

---
+++ php/constantes.php
@@ -0,0 +1,29 @@
+<?php
+	//define("PATH_TO_MEEKRODB", "./meekrodb/db.class.php");
+	define("PATH_TO_CLASS", "../php/class");
+
+	// Chemin du dossier
+	//define("BDD_CONFIG","./config/bddConfig.php");
+
+	// Utilisateurs
+	define("PSEUDO_MIN_SIZE", 6);
+	define("PSEUDO_MAX_SIZE", 20);
+
+	// mails
+	define("PSEUDO_FROM","Exercices de maths");
+	define("EMAIL_FROM","exercices@goupill.fr");
+	define("PATH_TO_SITE","http://tresor.goupill.fr");
+	define("NOM_SITE","Chasse au trésor sur goupill.fr");
+	define("SMTP_PASSWORD","zwdHpjudYfv3CbEbt8Aa");
+	define("SMTP_USER","exercices@goupill.fr");
+	define("SMTP_HOST","mail.goupill.fr");
+	define("SMTP_PORT",465);
+
+	// debug
+	define("DEBUG",true);
+	define("DEBUG_TEMPLATES",true);
+	define("BDD_DEBUG_ON",true);
+
+
+
+?>
