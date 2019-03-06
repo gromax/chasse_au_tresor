b6884ba4f85dc10c09b2027778886d55d243c868
gromax <gomaxx@gmail.com>
2019-03-06 16:14:44 +0100

    Racine js de l'application client

---
+++ dev/initialize.js
@@ -0,0 +1,8 @@
+//import './styles/application.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  require('app').app.start();
+});
