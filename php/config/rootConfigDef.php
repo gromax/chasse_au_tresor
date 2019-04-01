6280ccbcac1ed569ca64e40a779d9d2977771f70
gromax <gomaxx@gmail.com>
2019-04-01 18:21:36 +0200

    rootConfig par défaut

---
+++ php/config/rootConfigDef.php
@@ -0,0 +1,4 @@
+<?php
+	define("ROOT_USERNAME", "root"); // login du root
+	define("ROOT_PASSWORD_HASH", '$2y$10$fmPgrO5wQJe7w5u5TUVbX.qpfHq495SFVUCwMYA8E8LGdUsHEhADe'); // password du root, par défaut = 'vide'
+?>
