f302b9a4361b70dae6b4d86c8f82b46e7971271a
gromax <gomaxx@gmail.com>
2019-04-01 18:17:06 +0200

    ajout font awesome

--- dev/initialize.js
+++ dev/initialize.js
@@ -1,5 +1,6 @@
 //import './styles/application.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/jquery-ui/themes/base/all.css';
 import 'bootstrap';
 
 
