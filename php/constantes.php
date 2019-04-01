c7fd159a62bec65c5e8e229e58adf98b19145186
gromax <gomaxx@gmail.com>
2019-04-01 18:20:25 +0200

    constantes.php

--- php/constantes.php
+++ php/constantes.php
@@ -1,28 +1,34 @@
 <?php
-	define("PATH_TO_MEEKRODB", "../vendor/sergeytsalkov/meekrodb/db.class.php");
-	define("PATH_TO_CLASS", "../php/class");
+  define("PATH_TO_MEEKRODB", "../vendor/sergeytsalkov/meekrodb/db.class.php");
+  define("PATH_TO_UPLOAD", "../up/");
+  define("PATH_TO_CLASS", "../php/class");
 
-	// Chemin du dossier
-	define("BDD_CONFIG","../php/config/bddConfig.php");
+  // Chemin du dossier
+  define("BDD_CONFIG","../php/config/bddConfig.php");
+  if (file_exists("../php/config/rootConfig.php")) {
+  	require_once("../php/config/rootConfig.php");
+  } else {
+  	require_once("../php/config/rootConfigDef.php");
+  }
 
-	// Utilisateurs
-	define("PSEUDO_MIN_SIZE", 6);
-	define("PSEUDO_MAX_SIZE", 20);
+  // Utilisateurs
+  define("PSEUDO_MIN_SIZE", 6);
+  define("PSEUDO_MAX_SIZE", 20);
 
-	// mails
-	define("PSEUDO_FROM","Exercices de maths");
-	define("EMAIL_FROM","exercices@goupill.fr");
-	define("PATH_TO_SITE","http://tresor.goupill.fr");
-	define("NOM_SITE","Chasse au trésor sur goupill.fr");
-	define("SMTP_PASSWORD","zwdHpjudYfv3CbEbt8Aa");
-	define("SMTP_USER","exercices@goupill.fr");
-	define("SMTP_HOST","mail.goupill.fr");
-	define("SMTP_PORT",465);
+  // mails
+  define("PSEUDO_FROM","Exercices de maths");
+  define("EMAIL_FROM","exercices@goupill.fr");
+  define("PATH_TO_SITE","http://tresor.goupill.fr");
+  define("NOM_SITE","Chasse au trésor sur goupill.fr");
+  define("SMTP_PASSWORD","zwdHpjudYfv3CbEbt8Aa");
+  define("SMTP_USER","exercices@goupill.fr");
+  define("SMTP_HOST","mail.goupill.fr");
+  define("SMTP_PORT",465);
 
-	// debug
-	define("DEBUG",true);
-	define("DEBUG_TEMPLATES",true);
-	define("BDD_DEBUG_ON",true);
+  // debug
+  define("DEBUG",true);
+  define("DEBUG_TEMPLATES",true);
+  define("BDD_DEBUG_ON",true);
 
 
 
