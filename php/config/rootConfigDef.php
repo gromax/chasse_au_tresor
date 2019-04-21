3e94caaa1384d873104a2efbb9d3556775bbb8cf
gromax <gomaxx@gmail.com>
2019-04-21 10:55:22 +0200

    remplacé le mot de passe par une version en clair. Pas de raison de l'avoir crypté ici

--- php/config/rootConfigDef.php
+++ php/config/rootConfigDef.php
@@ -1,4 +1,4 @@
 <?php
 	define("ROOT_USERNAME", "root"); // login du root
-	define("ROOT_PASSWORD_HASH", '$2y$10$fmPgrO5wQJe7w5u5TUVbX.qpfHq495SFVUCwMYA8E8LGdUsHEhADe'); // password du root, par défaut = 'vide'
+	define("ROOT_PASSWORD", 'vide'); // password du root, par défaut = 'vide'
 ?>
