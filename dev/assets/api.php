502095423b028f04bf319a93982f56c34799814f
gromax <gomaxx@gmail.com>
2019-04-23 14:18:04 +0200

    tab en espaces ajout d'un message pour le cas ou le json_encode renvoie une erreur, ce qui arrive s'il y a des caractères avec accents

--- dev/assets/api.php
+++ dev/assets/api.php
@@ -16,70 +16,81 @@ require_once "../php/constantes.php";
 require "../php/class/Router.php";
 
 if (file_exists("../php/config/bddConfig.php")) {
-	$router = Router::getInstance();
+  $router = Router::getInstance();
 
-	// session
-	$router->addRule('api/session', 'session', 'fetch', 'GET'); // Session active
-	$router->addRule('api/session/:id', 'session', 'insert', 'PUT'); // reconnexion
-	$router->addRule('api/session', 'session', 'insert', 'POST'); // Tentative de connexion
-	$router->addRule('api/session/:id', 'session', 'delete', 'DELETE'); // Déconnexion
-	$router->addRule('api/session/test', 'session', 'logged', 'GET'); // Vérifie l'état de connexion
+  // session
+  $router->addRule('api/session', 'session', 'fetch', 'GET'); // Session active
+  $router->addRule('api/session/:id', 'session', 'insert', 'PUT'); // reconnexion
+  $router->addRule('api/session', 'session', 'insert', 'POST'); // Tentative de connexion
+  $router->addRule('api/session/:id', 'session', 'delete', 'DELETE'); // Déconnexion
+  $router->addRule('api/session/test', 'session', 'logged', 'GET'); // Vérifie l'état de connexion
 
-	// redacteurs
-	$router->addRule('api/redacteurs/:id', 'redacteurs', 'fetch', 'GET');
-	$router->addRule('api/redacteurs/:id', 'redacteurs', 'delete', 'DELETE');
-	$router->addRule('api/redacteurs/:id', 'redacteurs', 'update', 'PUT');
-	$router->addRule('api/redacteurs', 'redacteurs', 'insert', 'POST');
-	//$router->addRule('api/redacteur/:id/init', 'users', 'forgottenWithId', 'POST');
+  // redacteurs
+  $router->addRule('api/redacteurs/:id', 'redacteurs', 'fetch', 'GET');
+  $router->addRule('api/redacteurs/:id', 'redacteurs', 'delete', 'DELETE');
+  $router->addRule('api/redacteurs/:id', 'redacteurs', 'update', 'PUT');
+  $router->addRule('api/redacteurs', 'redacteurs', 'insert', 'POST');
+  //$router->addRule('api/redacteur/:id/init', 'users', 'forgottenWithId', 'POST');
 
-	// redacteurs
-	$router->addRule('api/joueurs/:id', 'joueurs', 'fetch', 'GET');
-	$router->addRule('api/joueurs/:id', 'joueurs', 'delete', 'DELETE');
-	$router->addRule('api/joueurs/:id', 'joueurs', 'update', 'PUT');
-	$router->addRule('api/joueurs', 'joueurs', 'insert', 'POST');
+  // redacteurs
+  $router->addRule('api/joueurs/:id', 'joueurs', 'fetch', 'GET');
+  $router->addRule('api/joueurs/:id', 'joueurs', 'delete', 'DELETE');
+  $router->addRule('api/joueurs/:id', 'joueurs', 'update', 'PUT');
+  $router->addRule('api/joueurs', 'joueurs', 'insert', 'POST');
 
-	// evenements
-	$router->addRule('api/evenements/:id', 'evenements', 'fetch', 'GET');
-	$router->addRule('api/evenements/:id', 'evenements', 'delete', 'DELETE');
-	$router->addRule('api/evenements/:id', 'evenements', 'update', 'PUT');
-	$router->addRule('api/evenements', 'evenements', 'insert', 'POST');
+  // evenements
+  $router->addRule('api/evenements/:id', 'evenements', 'fetch', 'GET');
+  $router->addRule('api/evenements/:id', 'evenements', 'delete', 'DELETE');
+  $router->addRule('api/evenements/:id', 'evenements', 'update', 'PUT');
+  $router->addRule('api/evenements', 'evenements', 'insert', 'POST');
 
-	// evenements
-	$router->addRule('api/itemsEvenement/:id', 'itemsEvenement', 'fetch', 'GET');
-	$router->addRule('api/itemsEvenement/:id', 'itemsEvenement', 'delete', 'DELETE');
-	$router->addRule('api/itemsEvenement/:id', 'itemsEvenement', 'update', 'PUT');
-	$router->addRule('api/itemsEvenement', 'itemsEvenement', 'insert', 'POST');
+  // evenements
+  $router->addRule('api/itemsEvenement/:id', 'itemsEvenement', 'fetch', 'GET');
+  $router->addRule('api/itemsEvenement/:id', 'itemsEvenement', 'delete', 'DELETE');
+  $router->addRule('api/itemsEvenement/:id', 'itemsEvenement', 'update', 'PUT');
+  $router->addRule('api/itemsEvenement', 'itemsEvenement', 'insert', 'POST');
 
-	// images
-	$router->addRule('api/images/:id', 'images', 'fetch', 'GET');
-	$router->addRule('api/images/:id', 'images', 'delete', 'DELETE');
-	$router->addRule('api/images', 'images', 'insert', 'POST');
+  // images
+  $router->addRule('api/images/:id', 'images', 'fetch', 'GET');
+  $router->addRule('api/images/:id', 'images', 'delete', 'DELETE');
+  $router->addRule('api/images', 'images', 'insert', 'POST');
 
-	// parties
-	$router->addRule('api/parties/:id', 'parties', 'fetch', 'GET');
-	$router->addRule('api/parties/:id', 'parties', 'delete', 'DELETE');
-	$router->addRule('api/parties', 'parties', 'insert', 'POST');
+  // parties
+  $router->addRule('api/parties/:id', 'parties', 'fetch', 'GET');
+  $router->addRule('api/parties/:id', 'parties', 'delete', 'DELETE');
+  $router->addRule('api/parties', 'parties', 'insert', 'POST');
 
 
-	// data
-	$router->addRule('api/customData/:asks', 'data', 'customFetch', 'GET');
-	$router->addRule('api/customData/partie/:id', 'data', 'partieFetch', 'GET');
+  // data
+  $router->addRule('api/customData/:asks', 'data', 'customFetch', 'GET');
+  $router->addRule('api/customData/partie/:id', 'data', 'partieFetch', 'GET');
 
-	$response = $router->load();
+  $response = $router->load();
 } else {
-	$response = array("error" => "Le fichier bddConfig.php n'existe pas !");
-	EC::set_error_code(422);
+  $response = array("error" => "Le fichier bddConfig.php n'existe pas !");
+  EC::set_error_code(422);
 }
 
-
-EC::header(); // Doit être en premier !
 if ($response === false) {
-	echo json_encode(array("ajaxMessages"=>EC::messages()));
+  $jsonOutput =  json_encode(array("ajaxMessages"=>EC::messages()));
 } else {
-	if (isset($response["errors"]) && (count($response["errors"])==0)) {
-		unset($response["errors"]);
-	}
-	echo json_encode($response);
+  if (isset($response["errors"]) && (count($response["errors"])==0)) {
+    unset($response["errors"]);
+  }
+  $errorsMessages = EC::messages();
+  if (count($errorsMessages)>0)
+  {
+    $response['errorsMessages'] = $errorsMessages;
+  }
+  $jsonOutput = json_encode($response);
+
+  if ($jsonOutput===false)
+  {
+    EC::set_error_code(501);
+    $jsonOutput = json_encode(array("error"=>"Problème encodage json"));
+  }
 }
+EC::header(); // Doit être en premier !
+echo $jsonOutput;
 
 ?>
