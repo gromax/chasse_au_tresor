4aea0b460a9b14832c4cfc611a6ec68bfa8bb72c
gromax <gomaxx@gmail.com>
2019-05-16 22:58:20 +0200

    configuration email par défaut

---
+++ php/configDef/emailConfig.php
@@ -0,0 +1,10 @@
+<?php
+  define("PSEUDO_FROM","mon.domaine.fr");
+  define("EMAIL_FROM","jeu@mon.domaine.fr");
+  define("PATH_TO_SITE","https://mon.domaine.fr");
+  define("NOM_SITE","Chasse au trésor sur goupill.fr");
+  define("SMTP_PASSWORD","blablabla");
+  define("SMTP_USER","jeu@mon.domaine.fr");
+  define("SMTP_HOST","mail.mon.domaine.fr");
+  define("SMTP_PORT",200);
+?>
