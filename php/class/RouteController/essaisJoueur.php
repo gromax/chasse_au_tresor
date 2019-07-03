f3d825dbf8d14f5d776ce35d3e6319815538746f
Maxence Klein <gromaxx@gmail.com>
2019-07-04 00:22:34 +0200

    routage vers fonctions pour nombre d'essais ok

--- php/class/RouteController/essaisJoueur.php
+++ php/class/RouteController/essaisJoueur.php
@@ -4,6 +4,7 @@ namespace RouteController;
 use ErrorController as EC;
 use AuthController as AC;
 use BDDObject\EssaiJoueur;
+use BDDObject\Partie;
 
 class essaisJoueur
 {
@@ -68,6 +69,43 @@ class essaisJoueur
         }
         EC::set_error_code(501);
         return false;
+    }
+
+    public function getCount()
+    {
+        $ac = new AC();
+        if (!$ac->connexionOk())
+        {
+            EC::set_error_code(401);
+            return false;
+        }
+        if (!$ac->isJoueur()) {
+            EC::set_error_code(403);
+            return false;
+        }
+        $id = (integer) $this->params['id'];
+        $idJoueur = $ac->getLoggedUserId();
+        $partie=Partie::getObject($id);
+        if ($partie===null)
+        {
+            EC::set_error_code(404);
+            return false;
+        }
+        if ($partie->getIdProprietaire() !== $idJoueur)
+        {
+            var_dump($idJoueur);
+            var_dump($partie->getIdProprietaire());
+
+            EC::set_error_code(403);
+            return false;
+        }
+        $cnt = EssaiJoueur::getOkCount($id);
+        if ($cnt===null)
+        {
+            EC::set_error_code(501);
+            return false;
+        }
+        return array("cnt"=>$cnt);
     }
 
 
