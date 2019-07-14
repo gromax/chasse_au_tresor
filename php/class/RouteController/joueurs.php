ff2bdb5b3dac0cda8d8e6bf59dd7a7a4501aa7da
Maxence Klein <gromaxx@gmail.com>
2019-07-15 01:27:05 +0200

    exploitation du controlleur users

--- php/class/RouteController/joueurs.php
+++ php/class/RouteController/joueurs.php
@@ -3,7 +3,7 @@
 namespace RouteController;
 use ErrorController as EC;
 use AuthController as AC;
-use BDDObject\Joueur as Item;
+use BDDObject\User;
 
 class joueurs
 {
@@ -20,47 +20,6 @@ class joueurs
         $this->params = $params;
     }
 
-    public function fetch()
-    {
-        $ac = new AC();
-        if (!$ac->connexionOk())
-        {
-            EC::set_error_code(401);
-            return false;
-        }
-
-        $id = (integer) $this->params['id'];
-        if (!$ac->isRoot() && ($ac->getLoggedUserId()!==$id)) {
-            EC::set_error_code(403);
-            return false;
-        }
-
-
-        $user = Item::getObject($id);
-        if ($user===null)
-        {
-            EC::set_error_code(404);
-            return false;
-        }
-
-        return $user->getValues();
-    }
-
-    public function fetchList()
-    {
-        $ac = new AC();
-        if (!$ac->connexionOk())
-        {
-            EC::set_error_code(401);
-            return false;
-        }
-
-        if ($ac->isRoot()) return Item::getList(array("root"=>true));
-        EC::set_error_code(403);
-        return false;
-    }
-
-
     public function delete()
     {
         // Seul root peut effacer un joueur
@@ -76,7 +35,7 @@ class joueurs
             return false;
         }
         $id = (integer) $this->params['id'];
-        $redac = Item::getObject($id);
+        $redac = User::getObject($id);
         if ($redac === null)
         {
             EC::set_error_code(404);
@@ -96,7 +55,8 @@ class joueurs
         if (!$ac->connexionOk())
         {
             $data = json_decode(file_get_contents("php://input"),true);
-            $itAdd = new Item();
+            $data['isredac'] = 0;
+            $itAdd = new User();
             $validation = $itAdd->update_validation($data);
             if ($validation === true)
             {
@@ -113,6 +73,7 @@ class joueurs
         }
         else
         {
+            // On doit s'inscrire donc être off pour créer un joueur
             EC::set_error_code(403);
             return false;
         }
@@ -139,7 +100,7 @@ class joueurs
 
         $data = json_decode(file_get_contents("php://input"),true);
 
-        $userToMod = Item::getObject($id);
+        $userToMod = User::getObject($id);
         if ($userToMod==null)
         {
             EC::set_error_code(404);
@@ -149,6 +110,7 @@ class joueurs
         $validation = $userToMod->update_validation($data);
         if ($validation === true)
         {
+            unset($data["isredac"]); // ne peut être modifié
             $modOk=$userToMod->update($data);
             if ($modOk === true)
             {
@@ -166,62 +128,5 @@ class joueurs
             return array('errors' => $validation);
         }
     }
-
-    public function forgottenWithEmail()
-    {
-        if ((isset($_POST['email'])) && (Item::checkEMail($_POST['email'])))
-        {
-            $email = $_POST['email'];
-            $user = Item::getObjectWithKey("username",$email);
-
-            if ($user===null)
-            {
-                EC::set_error_code(404);
-                return false;
-            }
-
-            $hash = $user->addHashForPasswordLost();
-            if ($hash===false)
-            {
-                EC::set_error_code(501);
-                return false;
-            }
-            $mail = new PHPMailer(true);            // Passing `true` enables exceptions
-            try{
-                //Server settings
-                $mail->CharSet = 'UTF-8';
-                //$mail->SMTPDebug = 2;             // Enable verbose debug output
-                $mail->isSMTP();                    // Set mailer to use SMTP
-                $mail->Host = SMTP_HOST;            // Specify main and backup SMTP servers
-                $mail->SMTPAuth = true;             // Enable SMTP authentication
-                $mail->Username = SMTP_USER;        // SMTP username
-                $mail->Password = SMTP_PASSWORD;    // SMTP password
-                $mail->SMTPSecure = 'ssl';          // Enable TLS encryption, `ssl` also accepted
-                $mail->Port = SMTP_PORT;            // TCP port to connect to
-
-                //Recipients
-                $mail->setFrom(EMAIL_FROM, PSEUDO_FROM);
-                $arrUser = $user->getValues();
-                $mail->addAddress($email, $arrUser['nom']);     // Add a recipient
-
-                //Content
-                $mail->isHTML(true);                            // Set email format to HTML
-                $mail->Subject = "Mot de passe oublié";
-                $mail->Body    = "<b>".NOM_SITE.".</b> Vous avez oublié votre mot de passe. Suivez ce lien pour pour modifier votre mot de passe : <a href='".PATH_TO_SITE."/#joueur/forgotten/$hash'>Réinitialisation du mot de passe</a>.";
-                $mail->AltBody = NOM_SITE." Vous avez oublié votre mot de passe. Copiez ce lien dans votre navigateur pour vous connecter et modifier votre mot de passe : ".PATH_TO_SITE."/#joueur/forgotten/$hash";
-                $mail->send();
-            }   catch (Exception $e) {
-                EC::addError("Le message n'a pu être envoyé. Erreur :".$mail->ErrorInfo);
-                EC::set_error_code(501);
-                return false;
-            }
-            return array("message"=>"Email envoyé.");
-        }
-        EC::set_error_code(501);
-        return false;
-    }
-
-
-
 }
 ?>
--- php/class/RouteController/redacteurs.php
+++ php/class/RouteController/redacteurs.php
@@ -3,10 +3,7 @@
 namespace RouteController;
 use ErrorController as EC;
 use AuthController as AC;
-use BDDObject\Redacteur as Item;
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
+use BDDObject\User;
 
 class redacteurs
 {
@@ -23,47 +20,6 @@ class redacteurs
         $this->params = $params;
     }
 
-    public function fetch()
-    {
-        $ac = new AC();
-        if (!$ac->connexionOk())
-        {
-            EC::set_error_code(401);
-            return false;
-        }
-
-        $id = (integer) $this->params['id'];
-        if (!$ac->isRoot() && ($ac->getLoggedUserId()!==$id)) {
-            EC::set_error_code(403);
-            return false;
-        }
-
-
-        $user = Item::getObject($id);
-        if ($user===null)
-        {
-            EC::set_error_code(404);
-            return false;
-        }
-
-        return $user->getValues();
-    }
-
-    public function fetchList()
-    {
-        $ac = new AC();
-        if (!$ac->connexionOk())
-        {
-            EC::set_error_code(401);
-            return false;
-        }
-
-        if ($ac->isRoot()) return Item::getList(array("root"=>true));
-        EC::set_error_code(403);
-        return false;
-    }
-
-
     public function delete()
     {
         // Seul root peut effacer un rédacteur
@@ -79,7 +35,7 @@ class redacteurs
             return false;
         }
         $id = (integer) $this->params['id'];
-        $redac = Item::getObject($id);
+        $redac = User::getObject($id);
         if ($redac === null)
         {
             EC::set_error_code(404);
@@ -99,7 +55,8 @@ class redacteurs
         if ($ac->isRoot())
         {
             $data = json_decode(file_get_contents("php://input"),true);
-            $itAdd = new Item();
+            $data['isredac'] = 1;
+            $itAdd = new User();
             $validation = $itAdd->update_validation($data, true);
             if ($validation === true)
             {
@@ -144,7 +101,7 @@ class redacteurs
 
         $data = json_decode(file_get_contents("php://input"),true);
 
-        $userToMod = Item::getObject($id);
+        $userToMod = User::getObject($id);
         if ($userToMod==null)
         {
             EC::set_error_code(404);
@@ -154,6 +111,7 @@ class redacteurs
         $validation = $userToMod->update_validation($data);
         if ($validation === true)
         {
+            unset($data["isredac"]); // ne peut être modifié
             $modOk=$userToMod->update($data);
             if ($modOk === true)
             {
@@ -171,60 +129,5 @@ class redacteurs
             return array('errors' => $validation);
         }
     }
-
-    public function forgottenWithEmail()
-    {
-        if ((isset($_POST['email'])) && (Item::checkEMail($_POST['email'])))
-        {
-            $email = $_POST['email'];
-            $user = Item::getObjectWithKey("username",$email);
-
-            if ($user===null)
-            {
-                EC::set_error_code(404);
-                return false;
-            }
-
-            $hash = $user->addHashForPasswordLost();
-            if ($hash===false)
-            {
-                EC::set_error_code(501);
-                return false;
-            }
-            $mail = new PHPMailer(true);            // Passing `true` enables exceptions
-            try{
-                //Server settings
-                $mail->CharSet = 'UTF-8';
-                //$mail->SMTPDebug = 2;             // Enable verbose debug output
-                $mail->isSMTP();                    // Set mailer to use SMTP
-                $mail->Host = SMTP_HOST;            // Specify main and backup SMTP servers
-                $mail->SMTPAuth = true;             // Enable SMTP authentication
-                $mail->Username = SMTP_USER;        // SMTP username
-                $mail->Password = SMTP_PASSWORD;    // SMTP password
-                $mail->SMTPSecure = 'ssl';          // Enable TLS encryption, `ssl` also accepted
-                $mail->Port = SMTP_PORT;            // TCP port to connect to
-
-                //Recipients
-                $mail->setFrom(EMAIL_FROM, PSEUDO_FROM);
-                $arrUser = $user->getValues();
-                $mail->addAddress($email, $arrUser['nom']);     // Add a recipient
-
-                //Content
-                $mail->isHTML(true);                            // Set email format to HTML
-                $mail->Subject = "Mot de passe oublié";
-                $mail->Body    = "<b>".NOM_SITE.".</b> Vous avez oublié votre mot de passe. Suivez ce lien pour pour modifier votre mot de passe : <a href='".PATH_TO_SITE."/#redacteur/forgotten/$hash'>Réinitialisation du mot de passe</a>.";
-                $mail->AltBody = NOM_SITE." Vous avez oublié votre mot de passe. Copiez ce lien dans votre navigateur pour vous connecter et modifier votre mot de passe : ".PATH_TO_SITE."/#redacteur/forgotten/$hash";
-                $mail->send();
-            }   catch (Exception $e) {
-                EC::addError("Le message n'a pu être envoyé. Erreur :".$mail->ErrorInfo);
-                EC::set_error_code(501);
-                return false;
-            }
-            return array("message"=>"Email envoyé.");
-        }
-        EC::set_error_code(501);
-        return false;
-    }
-
 }
 ?>
