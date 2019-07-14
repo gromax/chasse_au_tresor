3504fb9c274b83dbf8e7cdc435842e01e1e8b2d2
Maxence Klein <gromaxx@gmail.com>
2019-07-15 01:26:41 +0200

    exploitation de la classe User

--- php/class/BDDObject/Joueur.php
+++ php/class/BDDObject/Joueur.php
@@ -7,144 +7,25 @@ use ErrorController as EC;
 use SessionController as SC;
 use MeekroDBException;
 
-use BDDObject\Partie;
-
-final class Joueur extends Item
+class Joueur extends User
 {
-	protected static $BDDName = "joueurs";
-	protected static $privates = "hash";
+  ##################################### METHODES STATIQUES #####################################
 
-	##################################### METHODES STATIQUES #####################################
+  public static function getList($options = array())
+  {
+    require_once BDD_CONFIG;
+    try {
+      if (isset($options['root']))
+        return DB::query("SELECT j.id, j.nom, j.username, (SELECT COUNT(p.id) FROM ".PREFIX_BDD."parties p where p.idProprietaire = j.id) AS count_parties FROM ".PREFIX_BDD."users j WHERE j.isredac=0");
+      else
+        return array();
+    } catch(MeekroDBException $e) {
+      if (DEV) return array('error'=>true, 'message'=>"#User/getList : ".$e->getMessage());
+      return array('error'=>true, 'message'=>'Erreur BDD');
+    }
+  }
 
-	protected static function champs()
-	{
-		return array(
-			'nom' => array( 'def' => "", 'type' => 'string'),	// nom du joueur
-			'username' => array( 'def' => "", 'type'=> 'string'),	// username ou identifiant du joueur
-			'hash' => array( 'def' => "", 'type'=> 'string'),	// hash du mdp
-			);
-	}
-
-	public static function checkPwd($pwd)
-	{
-		return true;
-	}
-
-	public static function checkEMail($email)
-	{
-		return preg_match("#^[a-zA-Z0-9_-]+(.[a-zA-Z0-9_-]+)*@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$#", $email);
-	}
-
-	public static function usernameExists($username)
-	{
-		if ($username==ROOT_USERNAME)
-			return true;
-
-		require_once BDD_CONFIG;
-		try {
-			// Vérification que l'username
-			$results = DB::query("SELECT id FROM ".PREFIX_BDD."joueurs WHERE username=%s",$username);
-			if (DB::count()>0) return $results[0]["id"];
-		} catch(MeekroDBException $e) {
-			EC::addBDDError($e->getMessage());
-		}
-		return false;
-	}
-
-	public static function getList($options = array())
-	{
-		require_once BDD_CONFIG;
-		try {
-			if (isset($options['root']))
-				return DB::query("SELECT j.id, j.nom, j.username, (SELECT COUNT(p.id) FROM ".PREFIX_BDD."parties p where p.idProprietaire = j.id) AS count_parties FROM ".PREFIX_BDD."joueurs j");
-			else
-				return array();
-		} catch(MeekroDBException $e) {
-			if (DEV) return array('error'=>true, 'message'=>"#Joueur/getList : ".$e->getMessage());
-			return array('error'=>true, 'message'=>'Erreur BDD');
-		}
-	}
-
-	##################################### METHODES #####################################
-
-	public function update_validation($modifs=array())
-	{
-		if(isset($modifs['username']))
-		{
-			$username = $modifs['username'];
-		}
-		else
-		{
-			$username = false;
-		}
-		$errors = array();
-		// $username == false et $this->values['username']==="" -> erreur
-		// test uniquement si $username !==false et $username !== $this->values['username']
-		if (($username===false)&&($this->values['username']===""))
-		{
-			$errors['username'] = "L'identifiant / email indéfini.";
-		}
-		elseif (($username!==false)&&($username!=$this->values['username'])&&(self::usernameExists($username)!==false ))
-		{
-			$errors['username'] = "L'identifiant / email existe déjà.";
-		}
-		if (($this->values['hash']==="") && !isset($modifs['pwd']))
-		{
-			$errors['pwd'] = "Mot de passe invalide";
-		}
-		if (isset($modifs['pwd']) && (self::checkPwd($modifs['pwd'])!==true) )
-		{
-			$errors['pwd'] = "Mot de passe invalide";
-		}
-		if (count($errors)>0)
-		{
-			return $errors;
-		}
-		else
-		{
-			return true;
-		}
-	}
-
-	public function parseBeforeUpdate($modifs)
-	{
-		if (isset($modifs['pwd']))
-			$modifs['hash'] = password_hash($modifs['pwd'],PASSWORD_DEFAULT);
-			unset($modifs['pwd']);
-		return $modifs;
-	}
-
-	public function customDelete()
-	{
-		$options = array("joueur"=>$this->id);
-		return Partie::deleteList($options);
-	}
-
-	public function addHashForPasswordLost()
-	{
-		$hash = bin2hex(random_bytes(20));
-		$values = array( "hash"=>$hash, "idProprietaire"=>$this->id, "type"=>JOUEUR_PWD_LOST);
-		require_once BDD_CONFIG;
-		try {
-			DB::insert(PREFIX_BDD."hashs", $values);
-			return $hash;
-		} catch(MeekroDBException $e) {
-			EC::addBDDError($e->getMessage());
-		}
-		return false;
-	}
-
-	public function resetHashsForPasswordLost()
-	{
-		require_once BDD_CONFIG;
-		try {
-			DB::delete(PREFIX_BDD."hashs", "idProprietaire=%i AND type=".JOUEUR_PWD_LOST, $this->id);
-			return true;
-		} catch(MeekroDBException $e) {
-			EC::addBDDError($e->getMessage());
-		}
-		return false;
-	}
+  ##################################### METHODES #####################################
 
 }
 
--- php/class/BDDObject/Redacteur.php
+++ php/class/BDDObject/Redacteur.php
@@ -7,136 +7,22 @@ use ErrorController as EC;
 use SessionController as SC;
 use MeekroDBException;
 
-use BDDObject\Evenement;
-
-
-final class Redacteur extends Item
+class Redacteur extends User
 {
-	protected static $BDDName = "redacteurs";
-	protected static $privates = "hash";
-
-	##################################### METHODES STATIQUES #####################################
-
-	protected static function champs()
-	{
-		return array(
-			'nom' => array( 'def' => "", 'type' => 'string'),	// nom du rédacteur
-			'username' => array( 'def' => "", 'type'=> 'string'),	// username du rédacteur
-			'hash' => array( 'def' => "", 'type'=> 'string'),	// hash du mdp
-			);
-	}
-
-	public static function checkPwd($pwd)
-	{
-		return true;
-	}
-
-	public static function checkEMail($email)
-	{
-		return preg_match("#^[a-zA-Z0-9_-]+(.[a-zA-Z0-9_-]+)*@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$#", $email);
-	}
-
-	public static function usernameExists($username)
-	{
-		if ($username==ROOT_USERNAME)
-			return true;
-
-		require_once BDD_CONFIG;
-		try {
-			// Vérification que l'username
-			$results = DB::query("SELECT id FROM ".PREFIX_BDD."redacteurs WHERE username=%s",$username);
-			if (DB::count()>0) return $results[0]["id"];
-		} catch(MeekroDBException $e) {
-			EC::addBDDError($e->getMessage());
-		}
-		return false;
-	}
-
-	public static function getList($options = array())
-	{
-		require_once BDD_CONFIG;
-		try {
-			if (isset($options['root']))
-				return DB::query("SELECT r.id, r.nom, r.username, (SELECT COUNT(e.id) FROM ".PREFIX_BDD."evenements e where e.idProprietaire = r.id) AS count_evenements FROM ".PREFIX_BDD."redacteurs r");
-			else
-				return array();
-		} catch(MeekroDBException $e) {
-			if (DEV) return array('error'=>true, 'message'=>"#Redacteur/getList : ".$e->getMessage());
-			return array('error'=>true, 'message'=>'Erreur BDD');
-		}
-	}
-
-
-	##################################### METHODES #####################################
-
-	public function update_validation($modifs=array(), $full=false)
-	{
-		if(isset($modifs['username']))
-			$username = $modifs['username'];
-		elseif ($full)
-			$username = $this->values['username'];
-		else
-			$username = false;
-		$errors = array();
-		if ($username!==false){
-			$username_errors = array();
-			if (!self::checkEMail($username))
-				$username_errors[] = "Email invalide.";
-			if (($username!==false)&&($username!=$this->values['username'])&&(self::usernameExists($username)!==false ))
-				$username_errors[] = "L'identifiant / email existe déjà.";
-			if (count($username_errors)>0)
-				$errors['username'] = $username_errors;
-		}
-		if ($full && ($this->values['hash']==="") && !isset($modifs['pwd']))
-			$errors['pwd'] = "Mot de passe invalide";
-		if (isset($modifs['pwd']) && !self::checkPwd($modifs['pwd']) )
-			$errors['pwd'] = "Mot de passe invalide";
-		if (count($errors)>0)
-			return $errors;
-		else
-			return true;
-	}
-
-	public function parseBeforeUpdate($modifs)
-	{
-		if (isset($modifs['pwd']))
-			$modifs['hash'] = password_hash($modifs['pwd'],PASSWORD_DEFAULT);
-			unset($modifs['pwd']);
-		return $modifs;
-	}
-
-	public function customDelete()
-	{
-		$options = array("redacteur"=>$this->id);
-		return Evenement::deleteList($options);
-	}
-
-	public function addHashForPasswordLost()
-	{
-		$hash = bin2hex(random_bytes(20));
-		$values = array( "hash"=>$hash, "idProprietaire"=>$this->id, "type"=>REDACTEUR_PWD_LOST);
-		require_once BDD_CONFIG;
-		try {
-			DB::insert(PREFIX_BDD."hashs", $values);
-			return $hash;
-		} catch(MeekroDBException $e) {
-			EC::addBDDError($e->getMessage());
-		}
-		return false;
-	}
-
-	public function resetHashsForPasswordLost()
-	{
-		require_once BDD_CONFIG;
-		try {
-			DB::delete(PREFIX_BDD."hashs", "idProprietaire=%i AND type=".REDACTEUR_PWD_LOST, $this->id);
-			return true;
-		} catch(MeekroDBException $e) {
-			EC::addBDDError($e->getMessage());
-		}
-		return false;
-	}
-
+  public static function getList($options = array())
+  {
+    require_once BDD_CONFIG;
+    try {
+      if (isset($options['root']))
+        return DB::query("SELECT r.id, r.nom, r.username, (SELECT COUNT(e.id) FROM ".PREFIX_BDD."evenements e where e.idProprietaire = r.id) AS count_evenements FROM ".PREFIX_BDD."users r WHERE r.isredac=1");
+      else
+        return array();
+    } catch(MeekroDBException $e) {
+      if (DEV) return array('error'=>true, 'message'=>"#User/getList : ".$e->getMessage());
+      return array('error'=>true, 'message'=>'Erreur BDD');
+    }
+  }
+  ##################################### METHODES #####################################
 }
 
 ?>
