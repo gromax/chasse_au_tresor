b4e13cd5219c0793f6974411ad424a043536c81c
Maxence Klein <gromaxx@gmail.com>
2019-07-19 20:58:06 +0200

    ajout des routes pour partage

--- php/routes.php
+++ php/routes.php
@@ -58,7 +58,17 @@ function loadRouter()
   // forgotten
   $router->addRule('api/user/forgotten', 'users', 'forgottenWithEmail', 'POST');
   $router->addRule('api/user/forgotten/:hash', 'session', 'getUserSessionWithHash', 'GET');
+
+  // partages
+  $router->addRule('api/partages', 'partages', 'insert', 'POST');
+  $router->addRule('api/partages/:id', 'partages', 'insert', 'PUT');
+  $router->addRule('api/partages/list', 'partages', 'fetchList', 'GET');
+  $router->addRule('api/partages/:id', 'partages', 'delete', 'DELETE');
+  $router->addRule('api/evenement/:id/partages', 'partages', 'fetchEventList', 'GET');
+
   return $router;
+
+
 }
 
 ?>
